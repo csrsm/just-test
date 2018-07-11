2e59b3670ba9b71e1e2981a7fffd4afc58588d0e
cuiming <cuiming@zytcft.com>
2018-07-11 14:47:35 +0800

    spring

---
+++ house-work/src/main/java/com/newbee/HouseWorkApplication.java
@@ -0,0 +1,12 @@
+package com.newbee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouseWorkApplication.class, args);
+	}
+}
